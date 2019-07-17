9c5ea96f0237b4dde5a338295fdb164ae4c0a9e0
Andreas Büttner <andbue@gmail.com>
2019-07-17 17:53:24 +0200

    Removed everything but line segmentation

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ from __future__ import absolute_import, division, print_function
 from setuptools import setup
 
 setup(
+    version='0.1',
     include_package_data=True,
-    test_suite="nose.collector",
-    tests_require=['nose', 'hocr-spec'],
-    setup_requires=['pbr'],
-    pbr=True,
 )
